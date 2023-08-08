b6901c003b73095424b9977e5ffd6b3bfda5d1a7
Krivtsov <sevvmish@gmail.com>
2023-08-09 00:49:11 +0300

    09-08-2023

--- Assets/Scripts/Managers/BotController.cs
+++ Assets/Scripts/Managers/BotController.cs
@@ -10,7 +10,6 @@ public class BotController : MonoBehaviour
     private Rigidbody _rigidbody;
     private Transform kartTransform;
     private Transform nextPoint;
-    private Vector3 previousPoint;
     private float x_offsetOnStart;
 
     private float _timer;
@@ -25,8 +24,7 @@ public class BotController : MonoBehaviour
     }
 
     public void SetNextPoint(Transform nextPoint)
-    {
-        previousPoint = kartTransform.position;
+    {        
         this.nextPoint = nextPoint;
     }
 
@@ -37,68 +35,38 @@ public class BotController : MonoBehaviour
             kart.accelerate = true;
         }
 
-        if (_timer > updateDataTime)
+        updateDrivingData();
+    }
+
+    private void updateDrivingData()
+    {
+        if (nextPoint == null) return;
+
+        Vector3 newNextPosition = nextPoint.position + nextPoint.right * x_offsetOnStart;
+        float angle = Vector3.Angle(kartTransform.forward, (newNextPosition - kartTransform.position));
+        float distance = (newNextPosition - kartTransform.position).magnitude;
+        float x_step = kartTransform.InverseTransformPoint(newNextPosition).x;
+
+        if (angle > 2 && angle < 70 && (distance > 2))
         {
-            Vector3 newNextPosition = nextPoint.position + nextPoint.right * x_offsetOnStart;
-            float angle = Vector3.Angle(kartTransform.forward, (newNextPosition - kartTransform.position));
-            float distance = (newNextPosition - kartTransform.position).magnitude;
-            float x_step = kartTransform.InverseTransformPoint(newNextPosition).x;
-
-            //print(gameObject.name + ": angle - " + angle + ", dist - " + distance);
-
-            if (angle > 2 && angle < 70 && (distance > 2))
-            {                
-                if (x_step < 0)
-                {
-                    kart.Turn = -1;
-                }
-                else
-                {
-                    kart.Turn = 1;
-                }
-
-            }
-            else if (angle > 85 && kart.IsWallHelperOn)
+            if (x_step < 0)
             {
-                //print(gameObject.name + ": USEDUNSTUCK = angle - " + angle + ", dist - " + distance);
-                                
-                kart.transform.DOLookAt(new Vector3(newNextPosition.x, kartTransform.position.y, newNextPosition.z), 0.3f);
-
-                /*
-                if ((GameManager.Instance.MainPlayerTransform.position - kartTransform.position).magnitude < 15)
-                {
-                    kart.transform.DOLookAt(new Vector3(newNextPosition.x, kartTransform.position.y, newNextPosition.z), 0.3f);
-                }
-                else
-                {
-                    //kart.transform.LookAt(new Vector3(newNextPosition.x, kartTransform.position.y, newNextPosition.z));
-                    kart.transform.DOLookAt(new Vector3(newNextPosition.x, kartTransform.position.y, newNextPosition.z), 0.3f);
-                }
-                */
-
-                _rigidbody.velocity = Vector3.zero;
-
-                /*
-                float toYAngle = angle - 5;
-
-                if (x_step < 0)
-                {
-                    kart.Rigidbody.MoveRotation(Quaternion.AngleAxis(toYAngle, Vector3.up));
-                }
-                else
-                {
-                    kart.Rigidbody.MoveRotation(Quaternion.AngleAxis(-toYAngle, Vector3.up));
-                }*/
+                kart.Turn = -1;
             }
             else
             {
-                kart.Turn = 0;
+                kart.Turn = 1;
             }
+
+        }
+        else if (angle > 85 && kart.IsWallHelperOn)
+        {
+            kart.transform.DOLookAt(new Vector3(newNextPosition.x, kartTransform.position.y, newNextPosition.z), 0.3f);
+            _rigidbody.velocity = Vector3.zero;
         }
         else
         {
-            _timer += Time.deltaTime;
+            kart.Turn = 0;
         }
-        
     }
 }
--- Assets/Scripts/Others/MainPlayerDrivingHelper.cs
+++ Assets/Scripts/Others/MainPlayerDrivingHelper.cs
@@ -26,27 +26,25 @@ public class MainPlayerDrivingHelper : MonoBehaviour
     }
 
     private void Update()
-    {        
-        if (_timer > updateDataTime)
+    {
+        checkKartStuck();
+    }
+
+    private void checkKartStuck()
+    {
+        if (nextPoint == null) return;
+
+        float angle = Vector3.Angle(kart.transform.forward, (nextPoint.position - kart.transform.position));
+
+        if (angle > 85 && (lastPosition - kart.transform.position).magnitude < 0.1f && kart.IsWallHelperOn)
         {
-            float angle = Vector3.Angle(kart.transform.forward, (nextPoint.position - kart.transform.position));
+            //print(gameObject.name + ": USEDUNSTUCK  on main = angle - " + angle);
 
-            if (angle > 85 && (lastPosition - kart.transform.position).magnitude < 0.1f && kart.IsWallHelperOn)
-            {
-                //print(gameObject.name + ": USEDUNSTUCK  on main = angle - " + angle);
-
-                //kart.transform.DOLookAt(nextPoint.position, 0.3f);
-                kart.transform.DOLookAt(new Vector3(nextPoint.position.x, kart.transform.position.y, nextPoint.position.z), 0.3f);
-                _rigidbody.velocity = Vector3.zero;
-            }
-
-            lastPosition = kart.transform.position;
-
-        }
-        else
-        {
-            _timer += Time.deltaTime;
+            //kart.transform.DOLookAt(nextPoint.position, 0.3f);
+            kart.transform.DOLookAt(new Vector3(nextPoint.position.x, kart.transform.position.y, nextPoint.position.z), 0.3f);
+            _rigidbody.velocity = Vector3.zero;
         }
 
+        lastPosition = kart.transform.position;
     }
 }
