82f0904bcb1ed5c532445f3e263ddbfd8e5fdb86
Krivtsov <sevvmish@gmail.com>
2023-08-12 01:14:21 +0300

    12-08-2023

--- Assets/Scripts/Managers/CameraController.cs
+++ Assets/Scripts/Managers/CameraController.cs
@@ -1,6 +1,8 @@
 using DG.Tweening;
+using KartGame.KartSystems;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
@@ -10,15 +12,19 @@ public class CameraController : MonoBehaviour
     private Camera mainCamera;
     private Transform cameraBody;
     private Transform playerTransform;
-    private Vector3 cameraPositionShift = new Vector3(0,3,-4);
+    private ArcadeKart playerKart;
+    private Vector3 cameraPositionShift = new Vector3(0,2f,-3f);//-4
     private Vector3 cameraRotationShift = new Vector3(20,0,0);
         
-    private readonly float timerCooldown = 0.2f;
+    private readonly float timerCooldown = 0.1f;
+    private Vector3 speed;
+    private float zCoord;
     
     public void SetCameraController(Camera _camera, Transform _player)
     {
         mainCamera = _camera;
         playerTransform = _player;
+        playerKart = _player.GetComponent<ArcadeKart>();
 
         GameObject g = new GameObject("Camera Body");
         cameraBody = g.transform;
@@ -34,11 +40,31 @@ public class CameraController : MonoBehaviour
     
     private void Update()
     {
+
+        zCoord = math.lerp(cameraPositionShift.z, 0, playerKart.LocalSpeed());
+
+        cameraBody.position = Vector3.SmoothDamp(
+            cameraBody.position,
+            playerTransform.position
+             + playerTransform.right * cameraPositionShift.x
+             + playerTransform.up * cameraPositionShift.y
+             + playerTransform.forward * zCoord,
+            ref speed,
+            timerCooldown
+            );      
+        cameraBody.LookAt(playerTransform.position + Vector3.up * 1.2f);
+        
+
+        /*
+        zCoord = math.lerp(cameraPositionShift.z, 0, playerKart.LocalSpeed());
+
+
         cameraBody.DOMove(playerTransform.position
              + playerTransform.right * cameraPositionShift.x
              + playerTransform.up * cameraPositionShift.y
              + playerTransform.forward * cameraPositionShift.z, timerCooldown);
 
         cameraBody.DOLookAt(playerTransform.position + Vector3.up * 1.7f, timerCooldown);
+        */
     }
 }
